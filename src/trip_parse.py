7e2912078a205d3b0ff2e376e9911fd1b6a33634
iandioch <iandioch11@gmail.com>
2018-02-15 19:41:44 +0000

    Add example_trips.json

--- src/trip_parse.py
+++ src/trip_parse.py
@@ -4,7 +4,7 @@ from jsonschema import validate
 with open('trip_element_schema.json', 'r') as f:
     schema = json.load(f)
 
-with open('example.json', 'r') as f:
+with open('example_trips.json', 'r') as f:
     doc = json.load(f)
 
 validate(doc, schema)
