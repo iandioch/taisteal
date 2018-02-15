48fc4e276b3b571cce546ff78bdecd12fbc25422
iandioch <iandioch11@gmail.com>
2018-02-15 20:23:10 +0000

    Improve validification tool

--- src/trip_parse.py
+++ src/trip_parse.py
@@ -1,10 +1,17 @@
 import json
+import sys
+
 from jsonschema import validate
 
-with open('trip_element_schema.json', 'r') as f:
-    schema = json.load(f)
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Please provide 2 files, separated by a space.')
+        print('The first file should be the trip element schema file.')
+        print('The second file should be the document you want to parse.')
+    else:
+        with open(sys.argv[1], 'r') as f:
+            schema = json.load(f)
+        with open(sys.argv[2], 'r') as f:
+            doc = json.load(f)
 
-with open('example_trips.json', 'r') as f:
-    doc = json.load(f)
-
-validate(doc, schema)
+        validate(doc, schema)
