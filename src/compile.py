20502d7c741c186e1500b64aec387a9a19041897
iandioch <iandioch11@gmail.com>
2018-02-16 12:40:31 +0000

    tabs -> spaces

--- src/compile.py
+++ src/compile.py
@@ -11,26 +11,28 @@ files in that directory and recursive subdirectories
 into one big Taistil json document, and return it.
 '''
 def compile_directory(path):
-	obj = {
-		'note': 'Auto-compiled from directory {}'.format(path),
-		'elements': [],
-	}
-	items = listdir(path)
-	for item in items:
-		sub_path = join(path, item)
-		if isfile(sub_path):
-			with open(sub_path, 'r') as f:
-				data = json.load(f)
-				obj['elements'].append(data)
-		else:
-			obj['elements'].append(compile_directory(sub_path))
-	return obj
-			
+    obj = {
+        'note': 'Auto-compiled from directory {}'.format(path),
+        'elements': [],
+    }
+    items = listdir(path)
+    for item in items:
+        sub_path = join(path, item)
+        if isfile(sub_path):
+            with open(sub_path, 'r') as f:
+                try:
+                    data = json.load(f)
+                    obj['elements'].append(data)
+                except Exception as e:
+                    print(e)
+        else:
+            obj['elements'].append(compile_directory(sub_path))
+    return obj
 
 if __name__ == '__main__':
-	if len(sys.argv) == 1:
-		print('Please provide a directory path.')
-	else:
-		path = ' '.join(sys.argv[1:])
-		compiled = compile_directory(path)
-		print(json.dumps(compiled, indent=4))
+    if len(sys.argv) == 1:
+        print('Please provide a directory path.')
+    else:
+        path = ' '.join(sys.argv[1:])
+        compiled = compile_directory(path)
+        print(json.dumps(compiled, indent=4))
