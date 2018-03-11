d9e3e2f04c71953c5fdc73d6cd76e06a341d70e7
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-03-11 17:25:26 +0000

    Add TripVisit

--- src/trip.py
+++ src/trip.py
@@ -1,7 +1,16 @@
 import json
 
 
-class TripElement:
+class TripObject:
+    def to_dict(self):
+        return {}
+
+
+    def __str__(self):
+        return json.dumps(self.to_dict())
+
+
+class TripElement(TripObject):
     def __init__(self):
         self.elements = []
         self.note = ""
@@ -10,13 +19,29 @@ class TripElement:
     def to_dict(self):
         d = {}
         d['elements'] = [e.to_dict() for e in self.elements]
-        d['note'] = self.note
-        d['mode'] = self.mode
+        if self.note:
+            d['note'] = self.note
+        if self.mode:
+            d['mode'] = self.mode
         return d
 
-    def __str__(self):
-        return json.dumps(self.to_dict())
 
+class TripVisit(TripObject):
+    def __init__(self):
+        self.location = ""
+        self.datetime = ""
+        self.note = ""
+
+
+    def to_dict(self):
+        d = {}
+        d['location'] = self.location
+        d['datetime'] = self.datetime
+        if self.note:
+            d['note'] = self.note
+        return d
+
+    
 
 if __name__ == "__main__":
     t = TripElement()
