8a5a8b2c7fa9a28e675de6c8dd132e3c8b86efad
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-03-11 17:32:15 +0000

    Add docs, move stuff to parent class

--- src/trip.py
+++ src/trip.py
@@ -1,36 +1,52 @@
 import json
 
 
+'''
+Parent class of items that make part of a Taistil trip.
+'''
 class TripObject:
+
+    def __init__(self):
+        self.note = ""
+
     def to_dict(self):
-        return {}
+        d = {}
+        if self.note:
+            d['note'] = self.note
+        return d
 
 
     def __str__(self):
         return json.dumps(self.to_dict())
 
 
+'''
+A leg of a Taistil trip. Can recursively contain other legs.
+'''
 class TripElement(TripObject):
+
     def __init__(self):
+        super(TripElement, self).__init__()
         self.elements = []
-        self.note = ""
         self.mode = ""
 
     def to_dict(self):
-        d = {}
+        d = super().to_dict()
         d['elements'] = [e.to_dict() for e in self.elements]
-        if self.note:
-            d['note'] = self.note
         if self.mode:
             d['mode'] = self.mode
         return d
 
 
+'''
+A check in to a single location as part of a Taistil trip.
+'''
 class TripVisit(TripObject):
+
     def __init__(self):
+        super(TripVisit, self).__init__()
         self.location = ""
         self.datetime = ""
-        self.note = ""
 
 
     def to_dict(self):
@@ -49,4 +65,6 @@ if __name__ == "__main__":
     s = TripElement()
     s.note = "well wdc"
     t.elements.append(s)
+    r = TripVisit()
+    t.elements.append(r)
     print(t)
