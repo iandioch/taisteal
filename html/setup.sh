6299eb14532e886764b99b83f4aaf7e42905b755
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-06-08 09:08:11 +0100

    Remove html dir

--- html/setup.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/henrythasler/Leaflet.Geodesic.git
-mkdir include
-cp Leaflet.Geodesic/Leaflet.Geodesic.js include/Leaflet.Geodesic.js
-rm -rf Leaflet.Geodesic
--- html/webserver.py
+++
@@ -1,9 +0,0 @@
-import flask
-
-app = flask.Flask(__name__, static_url_path='')
-
-@app.route('/load/<path:path>')
-def static_files(path):
-    print(path)
-    return flask.send_from_directory('', path)
-app.run(host='0.0.0.0', port=8080)
