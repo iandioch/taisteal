2924db4144dbefdc038ac4fe53f3d55400245fb9
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-06-08 09:18:11 +0100

    Add comments to code

--- parser/parse.py
+++ parser/parse.py
@@ -3,13 +3,29 @@ import sys
 
 import jsonschema
 
+
+DEFAULT_SCHEMA_LOCATION = 'taisteal_schema.json'
+
+'''
+Validates the input json against the taisteal JSON schema
+at the given path. This ensures that the input JSON is in
+the right format.
+'''
 def validate_json_doc(json_data, json_schema_path):
     with open(json_schema_path, 'r') as f:
         schema = json.load(f)
     jsonschema.validate(json_data, schema)
 
+'''
+Returns a python object with the data contained in the
+input taisteal json document.
+'''
+def parse_json(json_data):
+    pass
+
 if __name__ == '__main__':
-    json_schema_path = 'taisteal_schema.json'
+
+    json_schema_path = DEFAULT_SCHEMA_LOCATION
     if len(sys.argv) > 1:
         json_schema_path = ' '.join(sys.argv[1:])
 
