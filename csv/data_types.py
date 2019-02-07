e476228c5f17e908a00152e1da6b2fbbeea7df91
Noah Ó Donnaile# <iandioch11@gmail.com>
2019-02-07 19:28:21 +0000

    Add basic statistics

--- csv/data_types.py
+++ csv/data_types.py
@@ -1,7 +1,10 @@
+import json
+from collections import defaultdict
+
 from location import Location
 
 class TravelLegPoint:
-    '''An arrival or departure (location, datetime) pair.'''
+    '''A (location, datetime) pair representing a single arrival or departure.'''
 
     def __init__(self, loc, date):
         '''Arguments:
@@ -12,10 +15,14 @@ class TravelLegPoint:
         print("Found location '{}' with result '{}'".format(loc, res))
         self.raw_date = date
 
+        # TODO(iandioch): Parse date.
+        self.date = self.raw_date
+
     def __repr__(self):
         return 'Point("{}")'.format(self.loc)
 
 class TravelLeg:
+    '''A single leg of travel, eg. one flight, or one bus journey.'''
 
     def __init__(self, csv_row):
         self.dep = TravelLegPoint(csv_row[0], csv_row[1])
@@ -24,3 +31,44 @@ class TravelLeg:
     def __repr__(self):
         return 'Leg({}, {})'.format(self.dep,
                                     self.arr)
+
+    def __gt__(self, other):
+        return self.dep.date > other.dep.date
+
+class TravelLegSeries:
+    '''A number of consecutive travel legs.'''
+
+    def __init__(self):
+        self.legs = []
+        self._compute_statistics()
+
+    def add_leg(self, leg):
+        self.legs.append(leg)
+        self.legs.sort()
+        self.stats.add_travel_leg(leg)
+
+    def _compute_statistics(self):
+        self.stats = TravelStatistics()
+
+class TravelStatistics:
+    def __init__(self):
+        self.num_legs = 0
+        self.country_to_num_visits = defaultdict(int)
+        self.locality_to_num_visits = defaultdict(int)
+
+    def add_travel_leg(self, leg):
+        self.add_travel_leg_point(leg.dep)
+        self.add_travel_leg_point(leg.arr)
+        self.num_legs += 1
+
+    def add_travel_leg_point(self, point):
+        print('Adding stats from travel leg point: "{}" on {}'.format(point.loc, point.date))
+        for component in point.loc.components:
+            print(component)
+            if 'country' in component['types']:
+                self.country_to_num_visits[component['short_name']] += 1
+            if 'locality' in component['types']:
+                self.locality_to_num_visits[component['short_name']] += 1
+
+    def __repr__(self):
+        return json.dumps(self.__dict__)
--- csv/parse.py
+++ csv/parse.py
@@ -1,17 +1,17 @@
 import location
-from data_types import TravelLeg
+from data_types import TravelLeg, TravelLegSeries
 
 import csv
 import sys
 
 CSV_COLUMNS = ['from_loc', 'from_date', 'to_loc', 'to_date', 'mode', 'note']
 
-def process_csv_row(row):
+def parse_travel_leg(row):
     # row can be assumed to match CSV_COLUMNS types.
-    leg = TravelLeg(row)
-    print(leg)
+    return TravelLeg(row)
 
 def main():
+    travel_legs = TravelLegSeries()
     location.load_location_lookup_cache()
     # Take input from stdin
     csv_reader = csv.reader(sys.stdin)
@@ -19,7 +19,9 @@ def main():
         if len(row) != len(CSV_COLUMNS):
             print('Error: wrong number of columns in csv.')
             continue
-        process_csv_row(row)
+        leg = parse_travel_leg(row)
+        travel_legs.add_leg(leg)
+    print(travel_legs.stats)
 
 if __name__ == '__main__':
     main()
