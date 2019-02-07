4d79b192c050395eac0b978a92c02fa896e809e1
Noah Ó Donnaile# <iandioch11@gmail.com>
2019-02-07 18:36:15 +0000

    Add Location class to CSV parser

--- csv/travel_leg.py
+++ csv/travel_leg.py
@@ -1,13 +1,30 @@
+class Location:
+    '''A single physical location'''
+
+    def __init__(self, raw_str):
+        # raw_str is the exact string the user inputted.
+        # Should not be used for anything other than debugging?
+        self.raw_str = raw_str
+
+        # cleaned_str is the stripped version of the raw_str.
+        self.cleaned_str = raw_str.strip()
+
+    def __repr__(self):
+        return self.cleaned_str
+
 class TravelLegPoint:
     '''An arrival or departure (location, datetime) pair.'''
 
     def __init__(self, loc, date):
         '''Arguments:
 
-        - loc (str): An address.
+        - loc (str): A raw address.
         - date (str): A datetime'''
-        self.raw_loc = loc
+        self.raw_loc = Location(loc)
         self.raw_date = date
+
+    def __repr__(self):
+        return 'Point("{}")'.format(self.raw_loc)
 
 class TravelLeg:
 
@@ -16,5 +33,5 @@ class TravelLeg:
         self.arr = TravelLegPoint(csv_row[2], csv_row[3])
 
     def __repr__(self):
-        return 'TravelLeg("{}", "{}")'.format(self.dep.raw_loc,
-                                              self.arr.raw_loc)
+        return 'Leg({}, {})'.format(self.dep.raw_loc,
+                                    self.arr.raw_loc)
