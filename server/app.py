09e57ce1be8a5895e0860bd21438d5354d5b7e54
Noah Ó Donnaile# <iandioch11@gmail.com>
2019-05-07 22:34:45 +0100

    serve stuff in app

--- server/app.py
+++ server/app.py
@@ -1,8 +1,14 @@
+import json
+
+from taisteal_csv import parse
 from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+with open('config.json') as f:
+    config = json.load(f)
 
 @app.route('/')
 def serve_root():
@@ -10,7 +16,15 @@ def serve_root():
 
 @app.route('/api/travel_map')
 def serve_travel_map():
+    loc = '../../mo_thaistil/2018_02_20_Lugano.csv'
+    data = parse.parse(loc)
     return '{}'
+
+@app.route('/api/get_mapbox_token')
+def serve_get_mapbox_token():
+    return json.dumps({
+        'token': config['mapbox_token']
+    })
 
 def main():
     app.run(port=1916)
