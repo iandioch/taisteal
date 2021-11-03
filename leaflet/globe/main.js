94a13ff52ffe7697cd4515370765909ccc07ea65
iandioch <iandioch11@gmail.com>
2021-11-03 16:27:22 +0000

    Draw legs between places

--- leaflet/globe/main.js
+++ leaflet/globe/main.js
@@ -22,18 +22,20 @@ function loadJSON(url, callback) {
 }
 
 (function() {
+    const GLOBE_RADIUS = 1;
     const canvas = document.querySelector('#globe-canvas');
     const renderer = new THREE.WebGLRenderer({canvas});
-    const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+    const camera = new THREE.PerspectiveCamera(45, 2, 0.025, 5);
     camera.position.z = 2;
     const controls = new OrbitControls(camera, canvas);
+    controls.minDistance = GLOBE_RADIUS*1.1;
+    controls.maxDistance = GLOBE_RADIUS*3;
+    controls.enablePan = false;
     const scene = new THREE.Scene();
-
-    const GLOBE_RADIUS = 1;
 
     { 
         // Create a sun and an ambient light.
-        const sunLight = new THREE.DirectionalLight(0xF9D72C, 1);
+        const sunLight = new THREE.DirectionalLight(0xF9D79C, 1);
         sunLight.position.set(-1, 2, 4);
         scene.add(sunLight);
         const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1);
@@ -47,7 +49,7 @@ function loadJSON(url, callback) {
     const textureLoader = new THREE.TextureLoader();
     textureLoader.load('land_ocean_ice_cloud_2048.jpg', (texture) => {
         // TODO(iandioch): it'd be cool here instead of drawing some picture of the earth to instead render the polygon for each country or something; if we did that, there's lots of cool interactions that could be added.
-        const globeGeometry = new THREE.SphereGeometry(GLOBE_RADIUS, 64, 64);
+        const globeGeometry = new THREE.SphereGeometry(GLOBE_RADIUS, 128, 128);
         const globeMaterial = new THREE.MeshPhongMaterial({ map: texture, overdraw: 0.5 });
         const globe = new THREE.Mesh(globeGeometry, globeMaterial);
         globeGroup.add(globe);
@@ -71,6 +73,26 @@ function loadJSON(url, callback) {
         vector.y = N * Math.sin(latRadians);
         console.log(lat, lng, "->", vector.x, vector.y, vector.z);
         return vector;
+    }
+
+    function drawArc(start, end, smoothness, width, colour) {
+        // Following https://stackoverflow.com/a/42721392
+        var cb = new THREE.Vector3();
+        var ab = new THREE.Vector3();
+        var normal = new THREE.Vector3();
+        cb.subVectors(new THREE.Vector3(), end);
+        ab.subVectors(start, end);
+        cb.cross(ab);
+        normal.copy(cb).normalize();
+
+        var angle = start.angleTo(end);
+        var angleDelta = angle/(smoothness-1);
+        var geom = new THREE.Geometry();
+        for (var i = 0; i < smoothness; i++) {
+            geom.vertices.push(start.clone().applyAxisAngle(normal, angleDelta*i));
+        }
+        var arc = new THREE.Line(geom, new THREE.LineBasicMaterial({color: colour, linewidth: width}));
+        globeGroup.add(arc);
     }
 
     function drawPoint(pos, radius, height, colour) {
@@ -109,18 +131,27 @@ function loadJSON(url, callback) {
         for (var i in data.visits) {
             const visit = data.visits[i];
             console.log(visit.num_visits);
-            var radius = 0.003;
+            var radius = 0.001;
             var colour = 0x55FF55;
             console.log(visit.num_visits*2, highestVisits);
             if (visit.num_visits >= highestVisits/3) {
                 colour = 0xAA3333;
-                radius = 0.008;
+                //radius = 0.008;
                 console.log(colour);
             } else if (visit.num_visits >= 2) {
                 colour = 0x5555FF;
-                radius = 0.005;
+                //radius = 0.005;
             }
-            drawPoint(latLngToVector(visit.location.lat, visit.location.lng), radius, 0.01 + visit.num_visits/200, colour);
+            drawPoint(latLngToVector(visit.location.lat, visit.location.lng), radius, 0.05 + visit.num_visits/100, colour);
+        }
+        for (var i in data.legs) {
+            const leg = data.legs[i];
+            if (leg.mode === "FILL") {
+                // These are auto-added to make all legs into one continuous route.
+                continue;
+            }
+            // TODO(iandioch): We could optimise by drawing shorter legs with a much lower smoothness.
+            drawArc(latLngToVector(leg.dep.lat, leg.dep.lng), latLngToVector(leg.arr.lat, leg.arr.lng), 64, leg.count, 0xFFFFFF);
         }
     });
 
@@ -137,6 +168,9 @@ function loadJSON(url, callback) {
 
     function render(time) {
         const seconds = time * 0.001;
+        // TODO(iandioch): When you get in close, the size of rendered points could change to show more detail.
+        const cameraDistance = camera.position.distanceTo(controls.target);
+        console.log(cameraDistance);
 
         if (resizeRendererToDisplaySize(renderer)) {
             const canvas = renderer.domElement;
@@ -144,7 +178,7 @@ function loadJSON(url, callback) {
             camera.updateProjectionMatrix();
         }
 
-        globeGroup.rotation.y = seconds/40.0;
+        //globeGroup.rotation.y = seconds/40.0;
 
         renderer.render(scene, camera);
         requestAnimationFrame(render);
