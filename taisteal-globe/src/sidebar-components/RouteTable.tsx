19bd3da96d7695585250fa767647d14738def3d9
Noah Ó Donnaile <iandioch11@gmail.com>
2023-08-08 21:22:28 +0200

    Add routetable pagination

--- taisteal-globe/src/sidebar-components/RouteTable.tsx
+++ taisteal-globe/src/sidebar-components/RouteTable.tsx
@@ -1,7 +1,7 @@
 import { Leg } from 'types'
 import { POILink } from 'sidebar-components/POILink'
 import { useState } from 'react'
-import { useReactTable, createColumnHelper, flexRender, getCoreRowModel, SortingState, getSortedRowModel } from '@tanstack/react-table'
+import { useReactTable, createColumnHelper, flexRender, getCoreRowModel, getPaginationRowModel, SortingState, getSortedRowModel, PaginationState } from '@tanstack/react-table'
 import { useSelector } from 'react-redux'
 import { RootState } from 'store'
 
@@ -10,6 +10,7 @@ type RouteTableProps = {
 };
 
 export const RouteTable = (props: RouteTableProps) => {
+    // TODO: When sidebar is hidden and re-shown, table resets pagination.
     const [sorting, setSorting] = useState<SortingState>([
         {
             id: 'count',
@@ -21,6 +22,11 @@ export const RouteTable = (props: RouteTableProps) => {
             desc: false,
         }
     ]); 
+
+    const [pagination, setPagination] = useState<PaginationState>({
+        pageIndex: 0,
+        pageSize: 10,
+    });
     const columnHelper = createColumnHelper<Leg>();
     const columns = [
         columnHelper.accessor(leg => leg.departureLocation, {
@@ -55,6 +61,7 @@ export const RouteTable = (props: RouteTableProps) => {
         state: {
             sorting,
         },
+        getPaginationRowModel: getPaginationRowModel(),
         onSortingChange: setSorting,
         getSortedRowModel: getSortedRowModel(),
     });
@@ -63,6 +70,7 @@ export const RouteTable = (props: RouteTableProps) => {
             <thead>
               {table.getHeaderGroups().map(headerGroup => (
                 <tr key={headerGroup.id}>
+                  <th colSpan={1}>#</th>
                   {headerGroup.headers.map(header => {
                     return (
                       <th key={header.id} colSpan={header.colSpan}>
@@ -92,8 +100,9 @@ export const RouteTable = (props: RouteTableProps) => {
               ))}
             </thead>
             <tbody>
-                {table.getRowModel().rows.map(row => (
+                {table.getRowModel().rows.map((row, rowIndex) => (
                 <tr key={row.id}>
+                  <td>{table.getState().pagination.pageIndex*table.getState().pagination.pageSize + rowIndex + 1}</td>
                   {row.getVisibleCells().map(cell => (
                     <td key={cell.id}>
                       {flexRender(cell.column.columnDef.cell, cell.getContext())}
@@ -102,6 +111,67 @@ export const RouteTable = (props: RouteTableProps) => {
                 </tr>
               ))}
             </tbody>
+            <div className="flex items-center gap-2">
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>>'}
+        </button>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
+            }}
+            className="border p-1 rounded w-16"
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={e => {
+            table.setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
         </table>
     </>);
 }
