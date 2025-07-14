d7c2b050f0d4276905a066b0f4977d41c1f4f9c7
Noah Ó Donnaile <iandioch11@gmail.com>
2025-07-14 13:56:17 +0000

    use docker volume for server sqlite db

--- server/build_and_run.sh
+++ server/build_and_run.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
 pwd
-docker run --rm -p 127.0.0.1:1916:1916 -it $(docker build -q .)
+docker run --mount type=volume,src=taisteal-data,dst=/app -p 127.0.0.1:1916:1916 -it $(docker build -q .)
