8ebdba03593e2675f68a6d828fbfb18bf4754c69
Noah Ó Donnaile <iandioch11@gmail.com>
2025-07-18 14:01:55 +0000

    Use createSelectors in /pois route so it doesn't re-render all the time

--- taisteal-globe/src/routes/pois.tsx
+++ taisteal-globe/src/routes/pois.tsx
@@ -8,14 +8,33 @@ import { VisitTable } from 'sidebar-components/VisitTable';
 import { loadMapData } from 'data';
 import { useEffect} from 'react';
 import { useSelector } from 'react-redux';
+import { createSelector } from '@reduxjs/toolkit';
 import { useParams } from 'react-router-dom';
+import { useThree } from '@react-three/fiber'
 import { RootState} from 'store';
 import { SidebarTunnel, SidebarHighlightTunnel  } from 'routes'
+import { lookAt } from 'action'
+import { Camera } from 'Camera'
+import { latLngToVector } from 'maths'
+import { Visit, Leg } from 'types'
+
+const getMatchingVisits = createSelector(
+    [(state: RootState) => state.visits.visits,
+    (state: RootState, id_set: Set<string>) => id_set],
+    (visits: Visit[], id_set) => visits.filter((visit) => id_set.has(visit.location.id)),
+)
+
+const getMatchingLegs = createSelector(
+    [(state: RootState) => state.legs.legs,
+    (state: RootState, id_set: Set<string>) => id_set],
+    (legs: Leg[], id_set) => legs.filter((leg) => id_set.has(leg.departureLocation.id) || id_set.has(leg.arrivalLocation.id)),
+)
 
 export default function POIGroup() {
   useEffect(() => { loadMapData(); });
 
   let {data} = useParams();
+  //let camera = useThree((state) => state.camera);
 
   // TODO: add some verification here that the string is the expected format.
   let id_list: string[] = [];
@@ -37,10 +56,21 @@ export default function POIGroup() {
   }
   const id_set = new Set<string>(id_list);
 
-  const matchingVisits = useSelector((state: RootState) => state.visits.visits.filter((visit) => id_set.has(visit.location.id)));
+  //const matchingVisits = useSelector((state: RootState) => state.visits.visits.filter((visit) => id_set.has(visit.location.id)));
+  //const matchingVisits = useSelector(getMatchingVisits, id_set);
+  const matchingVisits = useSelector((state: RootState) => getMatchingVisits(state, id_set));
+
+  //useEffect(() => {
+    const n = matchingVisits.length;
+    const latitude = matchingVisits.map((v) => v.location.latitude).reduce((a, b) => a+b, 0)/n; 
+    const longitude = matchingVisits.map((v) => v.location.longitude).reduce((a, b) => a+b, 0)/n;
+
+    //lookAt(camera, latitude, longitude);
+  //}, [matchingVisits]);
+  const cameraPos = latLngToVector(latitude, longitude, 2)
   console.log(matchingVisits);
 
-  const matchingLegs = useSelector((state: RootState) => state.legs.legs.filter((leg) => id_set.has(leg.departureLocation.id) || id_set.has(leg.arrivalLocation.id)));
+  const matchingLegs = useSelector((state: RootState) => getMatchingLegs(state, id_set));; //useSelector((state: RootState) => state.legs.legs.filter((leg) => id_set.has(leg.departureLocation.id) || id_set.has(leg.arrivalLocation.id)));
 
   // Locations that are not in this country but are connected.
   const connectedLocations = new Set<string>();
@@ -53,7 +83,7 @@ export default function POIGroup() {
     }
   }
 
-  const connectedVisits = useSelector((state: RootState) => state.visits.visits.filter((visit) => connectedLocations.has(visit.location.id)));
+  const connectedVisits = []; //useSelector((state: RootState) => state.visits.visits.filter((visit) => connectedLocations.has(visit.location.id)));
 
   function renderLegs() {
     return <>
@@ -65,6 +95,7 @@ export default function POIGroup() {
 
   return (
     <>
+        <Camera position={cameraPos} />
         {label && <>{renderLegs()}</>}
         {label && <MapPOIGroup visits={matchingVisits} cluster={false} />}
         <SidebarHighlightTunnel.In>
